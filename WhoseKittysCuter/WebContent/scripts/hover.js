42bbf0f1251020ce36af09587452617a8693f24f
Meg Parsons <meg.l.parsons@gmail.com>
2019-05-15 22:56:10 -0400

    submitted version

---
+++ WhoseKittysCuter/WebContent/scripts/hover.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ // takes two function for mouseenter and mouseleave
+	    $("img-item").hover(function(){
+	        $(this).addClass("highlight"); //adds a class
+	    }, function(){
+	        $(this).removeClass("highlight"); //removes 
+        });
+	 });
